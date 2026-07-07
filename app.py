913a074694e9faef961c0feb834fdc91cf276bf6
nyiry <dev@bejelentkezesek.mozmail.com>
2026-07-07 15:38:27 +0200

    setting up database URI

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
